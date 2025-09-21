257fa1f164d8782e8bdbf20ccd18d44aae63f237
Johnson Mauro <johnsonmauro@gmail.com>
2025-09-20 22:38:56 -0300

    WiP

--- src/pages/home/index.ts
+++
@@ -1,7 +0,0 @@
-// Home page sections
-export { HeroSection } from "./HeroSection";
-export { AboutSection } from "./AboutSection";
-export { BooksSection } from "./BooksSection";
-export { GallerySection } from "./GallerySection";
-export { ContactSection } from "./ContactSection";
-export { Footer } from "./Footer";
