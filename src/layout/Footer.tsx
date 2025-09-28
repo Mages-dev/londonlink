a46bad85ef9fbe913cae985f2d514af7a27f7ed6
Johnson Mauro <johnsonmauro@gmail.com>
2025-09-28 05:27:46 -0300

    WiP

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,12 +1,18 @@
-export function Footer() {
-  const currentYear = new Date().getFullYear();
+import Image from "next/image";
 
+export function Footer() {
   return (
     <footer className="mt-auto bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <p className="text-gray-600 dark:text-gray-300 text-lg font-bold drop-shadow-sm">
-          © {currentYear} LondonLink.
-        </p>
+        <div className="flex justify-center items-center">
+          <Image
+            src="/assets/images/shared/logos/mages-dev-logo.webp"
+            alt="Mages Dev Logo"
+            width={138}
+            height={46}
+            className="drop-shadow-sm"
+          />
+        </div>
       </div>
     </footer>
   );
